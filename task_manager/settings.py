72129e8fd489460b021b41ae85465a1661be515e
Glam52 <gla_max@outlook.com>
2024-10-14 10:52:11 +0700

    changing loading secret files

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -22,8 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-msgjn_y=#o&_xq_d^(0t3w2_wol7aeyx_w69^ik*6(9ho*h8un"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -101,12 +107,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "task_manager.wsgi.application"
 
 
-# Инициализация окружения
-env = environ.Env()
-environ.Env.read_env()  # Чтение файла .env
-
-# Получаем значение DATABASE_URL из переменной окружения
-DATABASE_URL = env('DATABASE_URL')
 # Конфигурация базы данных
 DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL)
