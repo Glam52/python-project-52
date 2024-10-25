adba8cfd48bd6c98489c84124b5c72e540d5a13e
Glam52 <gla_max@outlook.com>
2024-10-25 11:43:35 +0700

    added typehints in statuses\views.py

--- statuses/views.py
+++ statuses/views.py
@@ -8,38 +8,40 @@ from tasks.models import Task
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.http import HttpResponse, HttpRequest
+from typing import Any
 
 
 class StatusListView(LoginRequiredMixin, ListView):
     model = Status
-    template_name = "statuses/status_list.html"
-    context_object_name = "statuses"
+    template_name: str = "statuses/status_list.html"
+    context_object_name: str = "statuses"
 
 
 class StatusCreateView(SuccessMessageMixin, CreateView):
     model = Status
     form_class = StatusForm
-    template_name = "statuses/status_form.html"
-    success_url = reverse_lazy("statuses:list")
-    success_message = "Статус успешно создан"
+    template_name: str = "statuses/status_form.html"
+    success_url: str = reverse_lazy("statuses:list")
+    success_message: str = "Статус успешно создан"
 
 
 class StatusUpdateView(SuccessMessageMixin, UpdateView):
     model = Status
     form_class = StatusForm
-    template_name = "statuses/status_update.html"
-    success_url = reverse_lazy("statuses:list")
-    success_message = "Статус успешно изменен"
+    template_name: str = "statuses/status_update.html"
+    success_url: str = reverse_lazy("statuses:list")
+    success_message: str = "Статус успешно изменен"
 
 
 class StatusDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     model = Status
-    template_name = "statuses/status_confirm_delete.html"
-    success_url = reverse_lazy("statuses:list")
-    success_message = "Статус успешно удален"
+    template_name: str = "statuses/status_confirm_delete.html"
+    success_url: str = reverse_lazy("statuses:list")
+    success_message: str = "Статус успешно удален"
 
-    def post(self, request, *args, **kwargs):
-        self.object = self.get_object()
+    def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        self.object: Any = self.get_object()
 
         if Task.objects.filter(status=self.object).exists():
             messages.error(request,
